999527cdf022ac4b57386f11776e92dc2e053c6a
Eleftherios Batzolis <egbatzo@cs.ihu.gr>
2024-04-15 01:39:29 +0300

    minor changes

--- rtl/AnomalyDetectionSystem.v
+++ rtl/AnomalyDetectionSystem.v
@@ -1,15 +1,14 @@
 module i_tree(
     input wire clk,
     input wire reset,
-    input wire [7:0] data_input, // Assuming this is the correct input for FIFO
+    input wire [7:0] data_input,  // Assuming this is the correct input for FIFO
     input wire data_valid,
-    input wire [255:0] itree_input,
     output reg anomaly_detected
 );
 
 // Signals for FIFO buffer and control
 wire [7:0] fifo_data_out;
-reg fifo_read_enable; // Changed to reg if driven by procedural code, or keep as wire if driven by continuous assignment
+reg fifo_read_enable; // Changed to reg if driven by procedural code
 wire fifo_empty, fifo_full;
 wire internal_data_valid; // Internal signal to represent data validity based on FIFO status
 
@@ -17,7 +16,7 @@ wire internal_data_valid; // Internal signal to represent data validity based on
 InputBufferFIFO input_buffer (
     .clk(clk),
     .reset(reset),
-    .sensor_input(data_input), // Corrected to use data_input
+    .sensor_input(data_input),
     .read_enable(fifo_read_enable),
     .fifo_output(fifo_data_out),
     .fifo_empty(fifo_empty),
@@ -28,22 +27,18 @@ InputBufferFIFO input_buffer (
 IsolationTreeStateMachine isolation_tree_sm (
     .clk(clk),
     .reset(reset),
-    .data_input(fifo_data_out), // Assuming you want to use FIFO output here
+    .data_input(fifo_data_out), // Using FIFO output here
     .data_valid(internal_data_valid),
-    .load_itree(load_itree),
-    .itree_input(itree_input),
     .anomaly_detected(anomaly_detected)
 );
 
 // Control Logic for internal data validity and FIFO read enable
 assign internal_data_valid = !fifo_empty && data_valid; // Simplified condition
-// Define logic for fifo_read_enable based on your requirements
-// Example:
 always @(posedge clk or negedge reset) begin
     if (!reset) begin
         fifo_read_enable <= 0;
     end else begin
-        fifo_read_enable <= !fifo_empty && !anomaly_detected; // Example condition
+        fifo_read_enable <= !fifo_empty && !anomaly_detected; // Control reading based on FIFO status and anomaly detection
     end
 end
 
